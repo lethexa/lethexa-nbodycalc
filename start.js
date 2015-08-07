6b4a187b7d5d8bd8f970bfbc279a6f7f3a71c662
Tim Leerhoff <tleerhof@web.de>
2015-08-07 21:31:15 +0200

    Now seams to work

--- start.js
+++ start.js
@@ -4,15 +4,23 @@ var vecmat = require('lethexa-vecmat');
 var sim = new nbodycalc.GravitySimulator();
 
 var body1 = { 
-	position: vecmat.makeNullVector3d(),
+	position: new vecmat.Vector3d(1.0, 0.0, 0.0),
 	velocity: vecmat.makeNullVector3d(),
-	mass: 1.0
+	mass: 100000.0
+};
+
+var body2 = { 
+	position: new vecmat.Vector3d(-1.0, 0.0, 0.0),
+	velocity: vecmat.makeNullVector3d(),
+	mass: 100000.0
 };
 
 sim.addBody(body1);
+sim.addBody(body2);
 
-console.log(body1);
 sim.updateTime(1.0);
-console.log(body1);
+sim.updateTime(1.0);
+sim.updateTime(1.0);
+sim.updateTime(1.0);
 
 
