de96d1226abae88e5cf447bc20484dada54cb2bc
Tim Leerhoff <tleerhof@web.de>
2015-08-08 18:18:49 +0200

    Added second test

--- test/rungekutta.js
+++ test/rungekutta.js
@@ -4,7 +4,7 @@ var integrator = require('../lib/rungekutta');
 
 describe('RungeKutta', function () {
     describe('#nextStep()', function () {
-        it('should correct result', function () {
+        it('should return the correct result 1', function () {
 		var rungekutta = new integrator.RungeKutta( function(x, y) {
 			return 3 * x*x * y;
 		}); 
@@ -14,6 +14,18 @@ describe('RungeKutta', function () {
 
             	assert.equal(1.1, Math.round(result.x1 * 1000.0) / 1000.0);
             	assert.equal(2.785, Math.round(result.y1 * 1000.0) / 1000.0);
+        });
+     
+	it('should return the correct result 2', function () {
+		var rungekutta = new integrator.RungeKutta( function(t, v) {
+			return (1.0/1000.0 * (1000.0 - 10.0 * v)) * t;
+		}); 
+		var t0 = 0, v0 = 0, dt = 2.0;
+
+		var result = rungekutta.nextStep(t0, v0, dt);
+
+            	assert.equal(dt, Math.round(result.x1 * 1000.0) / 1000.0);
+            	assert.equal(1.98, Math.round(result.y1 * 1000.0) / 1000.0);
         });
     });
 });
